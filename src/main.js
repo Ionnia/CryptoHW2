d7661bab8090e79ec9dd47e8f1dd56febfbf81d6
Valentine <baymakanov@gmail.com>
2020-11-02 01:30:09 +0300

    Completed second homework

--- src/main.js
+++ src/main.js
@@ -1,3 +1,91 @@
-function openYourCar() {
+let jsrsasign = require('jsrsasign');
 
+class Participant {
+    constructor(selfPvtKey, otherPubKey) {
+        this.selfPvtKey = selfPvtKey;
+        this.otherPubKey = otherPubKey;
+    }
+
+    sign(data) {
+        let sig = new jsrsasign.KJUR.crypto.Signature({"alg": "SHA1withRSA"});
+
+        sig.init(this.selfPvtKey);
+        sig.updateString(data);
+
+        return sig.sign();
+    }
+
+    verify(data, sigValueHex) {
+        let sig = new jsrsasign.KJUR.crypto.Signature({"alg": "SHA1withRSA"});
+
+        sig.init(this.otherPubKey);
+        sig.updateString(data);
+
+        let isValid = sig.verify(sigValueHex);
+
+        if(isValid) {
+            this.otherChallenge = data.split("|")[1];
+        }
+
+        return isValid;
+    }
+
+    getChallenge() {
+        let random = "";
+        while(random.length < 256) {
+            random += Math.random().toString(36).slice(2,10);
+        }
+        this.lastChallenge = random;
+        return random;
+    }
 }
+
+console.log("Generating key pairs for the car and the trinket");
+// Генерация пары ключей для автомобиля и брелока
+const carKeypair = jsrsasign.KEYUTIL.generateKeypair("RSA", 2048);
+const trinketKeypair = jsrsasign.KEYUTIL.generateKeypair("RSA", 2048);
+console.log("Keys generated!");
+const Car = new Participant(carKeypair.prvKeyObj, trinketKeypair.pubKeyObj);
+const Trinket = new Participant(trinketKeypair.prvKeyObj, carKeypair.pubKeyObj);
+
+function openCar() {
+    let trinketMessage = "OPEN|" + Trinket.getChallenge();
+    let trinketSign = Trinket.sign(trinketMessage);
+    console.log("Trinket sends signed (command + challenge) to the car: " + trinketMessage);
+    console.log("Trinket's signature: " + trinketSign);
+
+    console.log("Car verifies trinket's message");
+    let isValid = Car.verify(trinketMessage, trinketSign);
+    if(!isValid) {
+        console.log("Validation failed");
+        return;
+    }
+
+    let carMessage = Car.otherChallenge + "|" + Car.getChallenge();
+    let carSign = Car.sign(carMessage);
+    console.log("Car sends signed (trinket's challenge + car's challenge) to the trinket: " + carMessage);
+    console.log("Car's signature: " + carSign);
+
+    console.log("Trinket verifies car's message");
+    isValid = Trinket.verify(carMessage, carSign);
+    if(!isValid || Trinket.lastChallenge !== carMessage.split("|")[0]) {
+        console.log("Validation failed");
+        return;
+    }
+
+    let trinketReply = "|" + Trinket.otherChallenge;
+    let replySign = Trinket.sign(trinketReply);
+    console.log("Trinket sends signed car's challenge to the car: " + trinketReply);
+    console.log("Trinket's signature: " + replySign);
+
+    console.log("Car verifies challenge from trinket");
+    isValid = Car.verify(trinketReply, replySign + 'a');
+    if(!isValid || Car.lastChallenge !== Car.otherChallenge) {
+        console.log("Validation failed");
+        return;
+    }
+
+    console.log("Car opens!");
+}
+
+openCar();
